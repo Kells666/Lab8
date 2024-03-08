f63a166d75b1026245e2b8be7705bfb522298a30
Kells Zhang <122254227+Kells666@users.noreply.github.com>
2024-03-08 15:10:14 -0700

    lab 8

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,16 +1,30 @@
 package com.example.lab8;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-//import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
+import android.content.Context;
+import org.mockito.Mockito;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
 
+    @BeforeEach
+    public void createMockList() {
+        Context context = Mockito.mock(Context.class); // Mock the Context as it's required by CustomList
+        list = new CustomList(context, new ArrayList<City>());
+    }
+
+
+    @Test
+    public void addCityTest() {
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize + 1);
+    }
+
+    // Any additional tests can be added here
 }
